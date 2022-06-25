1a8d2525765bc03fc54b78a4d752ae3729ba5ce9
EC2 Default User <ec2-user@ip-172-31-25-185.ap-northeast-1.compute.internal>
2022-06-25 03:01:44 +0000

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :basic_auth
+    
+    private
+    
+    def basic_auth
+        authenticate_or_request_with_http_basic do |username, password|
+            # 環境変数を使ってBasci認証を行えるユーザー名とパスワードを定義
+            username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+        end
+    end
 end
